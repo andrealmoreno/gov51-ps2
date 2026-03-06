6bd6683cd28c4dc82ccb5e9b58640ac44298e701
andrealmoreno <you@example.com>
2026-03-05 20:21:12 -0500

    Setup folders and added PS2.R script

---
+++ PS2.Rmd.R
@@ -0,0 +1,11 @@
+---
+title: "Problem Set 2: The LaCour-Green Study"
+author: "Andrea Moreno"
+date: "`r Sys.Date()`"
+output: pdf_document
+---
+  **GitHub Repository:** [https://github.com/your-username/gov51-ps2](https://github.com/your-username/gov51-ps2)
+---
+ 
+
+  
